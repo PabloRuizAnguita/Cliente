aebcd14e8ea1ab2d0b070ffa15afce76b9444fac
PabloRuizAnguita <pra0012@alu.medac.es>
2024-02-06 19:13:49 +0100

    nueva actu

--- js.js
+++ js.js
@@ -2,10 +2,43 @@
 
 function añadirT(){
     var input=document.getElementsByClassName("input")[0].value;
-    localStorage.setItem("id",input);
+    var lista = JSON.parse(localStorage.getItem("nombres")) || [];
+    lista.push(input);
+    localStorage.setItem("nombres", JSON.stringify(lista));
+    alert("Su nombre se ha añadido correctamente.");
+    document.getElementsByClassName("input")[0].value = "";
+    
 }
-function mostrarT() { 
-    var json = localStorage.getItem("id");
-    console.log(json);
+
+
+function cleanT() {
+    localStorage.removeItem("nombres");
+    alert("La plantilla ha sido eliminada.");
+    var tituloLista = document.getElementById("tituloLista");
+    tituloLista.style.display= "none";
+    mostrarT();
+}
+
+function eliminarNombre(i) {
+    var lista = JSON.parse(localStorage.getItem("nombres")) || [];
+    lista.splice(i, 1);
+    localStorage.setItem("nombres", JSON.stringify(lista));
+    console.log("eliminar",i);
+
+    mostrarT();
+}
+
+
+function mostrarT() {
+    var lista = JSON.parse(localStorage.getItem("nombres")) || [];
+    var listaHTML = "";
+    
+    for (var i = 0; i < lista.length; i++) {
+        listaHTML += "<li>" + lista[i] + ` <button id="boton-eliminar" onclick='eliminarNombre(${i})'>Eliminar</button></li>`;
+    }
+    
+    document.getElementById("lista").innerHTML = listaHTML;
+    var tituloLista = document.getElementById("tituloLista");
+    tituloLista.style.display= "block";
 }
 
